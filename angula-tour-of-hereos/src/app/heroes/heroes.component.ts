bb8ab337548e9d721de3d7a02f0b7012d5f087f5
Noel-Riedmann <138024930+Noel-Riedmann@users.noreply.github.com>
2023-06-29 16:04:00 +0200

    Chapter 4 completed

--- angula-tour-of-hereos/src/app/heroes/heroes.component.ts
+++ angula-tour-of-hereos/src/app/heroes/heroes.component.ts
@@ -1,23 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from 'c:/Users/noel1/Desktop/AngularJS/angula-tour-of-hereos/src/app/mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  hero: Hero = {
-    id: 0,
-    name: 'Windstorm'
-  };
-
-  heroes = HEROES;
-
+export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
